bcf2426eaff3554bd9f6bb731eb37b186a3051e7
Spencer <spencer.powers197@gmail.com>
2020-12-04 17:13:57 -0500

    Added initial file for encoding.py to convert DAVIS data into the four-channel network input

---
+++ encoding.py
@@ -0,0 +1,14 @@
+'''
+encoding.py - tools for encoding the event information
+from the DAVIS camera into the discretized input
+representation detailed in Lee et al.
+'''
+import numpy as np
+import h5py
+
+# data is a data_loader object, N is 
+# number of event frames per group
+def encode_inputs(data, N)
+
+
+
