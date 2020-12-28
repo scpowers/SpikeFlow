4c59618bde6282d4276a0082434876f1609265d0
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2020-12-28 16:53:16 -0500

    Updated frame encoding to accumulate events (cell values can go above 1), per the original authors' code

--- encoding.py
+++ encoding.py
@@ -5,36 +5,33 @@ representation detailed in Lee et al.
 '''
 import numpy as np
 import h5py
+from tqdm import tqdm
 
 # data is a data_loader object, N is 
 # number of event frames per group
 def encode_inputs(data, N):
 
-    # initialize arrays to be saved later
-    # note: assuming that polarities are not summed
-    # in each of the on/off frames...still binary.
-    # also, one fewer than num_images because frames only created between
-    # pairs of images...one fewer gap than the number of images
     '''
-    former_ON = np.zeros((data.num_images - 1, N, data.im_width, 
-        data.im_height))
-    former_OFF = np.zeros((data.num_images - 1, N, data.im_width, 
-        data.im_height))
-    latter_ON = np.zeros((data.num_images - 1, N, data.im_width, 
-        data.im_height))
-    latter_OFF = np.zeros((data.num_images - 1, N, data.im_width, 
-        data.im_height))
+    initialize arrays to be saved later
+    note: assuming that polarities are not summed
+    in each of the on/off frames...still binary.
+    also, one fewer than num_images because frames only created between
+    pairs of images...one fewer gap than the number of images
     '''
-    former_ON = np.zeros((5, N, data.im_width, data.im_height))
-    former_OFF = np.zeros((5, N, data.im_width, data.im_height))
-    latter_ON = np.zeros((5, N, data.im_width, data.im_height))
-    latter_OFF = np.zeros((5, N, data.im_width, data.im_height))
+    former_ON = np.zeros((49, N, data.im_width, data.im_height))
+    former_OFF = np.zeros((49, N, data.im_width, data.im_height))
+    latter_ON = np.zeros((49, N, data.im_width, data.im_height))
+    latter_OFF = np.zeros((49, N, data.im_width, data.im_height))
+
     # holds event frame times for each frame
-    frame_times = np.zeros((5, 2*N)) 
+    frame_times = np.zeros((49, 2*N)) 
+
+    # determine the total number of events available to encode
+    tot_events = len(data.events[0:data.image_raw_event_inds[50]])
+    num_grouped = 0
 
     # splits are largely governed by number of GS images
-    #for i in range(data.num_images-1):
-    for i in range(5): # only encode first 6 images
+    for i in tqdm(range(49)):
         # if first image, no corresponding nearest event
         if i == 0:
             event_i_start = 0
@@ -71,10 +68,10 @@ def encode_inputs(data, N):
         print('dt: ', dt)
         '''
 
-        num_grouped = 0
-
+        # loop over the four channels
         for n in range(N):
             for e in events:
+                # store info from each event
                 y = int(e[0])
                 x = int(e[1])
                 t = e[2]
@@ -83,53 +80,41 @@ def encode_inputs(data, N):
                 # former event frames
                 if t >= t_start + (n*dt) and t < t_start + ((n+1)*dt):
                     num_grouped += 1
+
                     # if you find an ON event
                     if p == 1:
-                        # only update event frame if an event wasn't already here
-                        if former_ON[i,n,x,y] != 1:
-                            former_ON[i,n,x,y] = 1
+                        # accumulate
+                        former_ON[i,n,x,y] += 1
+
                     # must be an OFF event then
                     else:
-                        # still only update if an event wasn't already here
-                        if former_OFF[i,n,x,y] != 1:
-                            former_OFF[i,n,x,y] = 1
+                        # accumulate
+                        former_OFF[i,n,x,y] += 1
 
                 # latter event frames
                 if e[2] >= t_mid + (n*dt) and e[2] < t_mid + ((n+1)*dt):
                     num_grouped += 1
+
                     # if you find an ON event
                     if p == 1:
-                        # only update event frame if an event wasn't already here
-                        if latter_ON[i,n,x,y] != 1:
-                            latter_ON[i,n,x,y] = 1
+                        # accumulate
+                        latter_ON[i,n,x,y] += 1
+
                     # must be an OFF event then
                     else:
-                        # still only update if an event wasn't already here
-                        if latter_OFF[i,n,x,y] != 1:
-                            latter_OFF[i,n,x,y] = 1
-        print('% grouped between images ', i, '-', i+1, ': ', 
-                num_grouped/len(events) * 100)
+                        # accumulate
+                        latter_OFF[i,n,x,y] += 1
 
+    # determine encoding quality
+    print('%% of events encoded: % 2.2f' % (num_grouped/tot_events * 100))
 
-    # now save arrays containing event frames
-    np.save(data.filename + '_former_ON.npy', former_ON)
-    np.save(data.filename + '_former_OFF.npy', former_OFF)
-    np.save(data.filename + '_latter_ON.npy', latter_ON)
-    np.save(data.filename + '_latter_OFF.npy', latter_OFF)
-
-    output = np.zeros((4,5, N, data.im_width, data.im_height))
+    # now save the four channels in a single network input file
+    output = np.zeros((4, 49, N, data.im_width, data.im_height))
     output[0,:,:,:,:] = former_ON
     output[1,:,:,:,:] = former_OFF
     output[2,:,:,:,:] = latter_ON
     output[3,:,:,:,:] = latter_OFF
     np.save(data.filename + '_network_input.npy', output)
     np.save(data.filename + '_frame_times.npy', frame_times)
-
-
-        
-
-
-
-
 
 
